f5fb76d1908a4e3e32ffef35e39e1f30ad9f3455
Icecream95 <the.real.icecream95@gmail.com>
2026-02-09 17:17:12 +1300

    Use enable --now to start the service

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ install -Dm644 x1e-ec-tool.service /usr/local/lib/systemd/system/x1e-ec-tool.ser
 modprobe i2c-dev
 
 systemctl daemon-reload
-systemctl enable x1e-ec-tool.service
-systemctl start x1e-ec-tool.service
+systemctl enable --now x1e-ec-tool.service
 sleep 1
 systemctl status x1e-ec-tool.service
